43cb15ae51488adde0f22010c510542d0afc4be3
Lucas Braga <130006732+LucasBraga2@users.noreply.github.com>
2023-11-10 10:35:55 -0300

    Create arvore.h

---
+++ arvore.h
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef int tp_item;
+
+typedef struct tp_no
+{
+    struct tp_no *esq;
+    tp_item info;
+    struct tp_no *dir;
+} tp_no;
+
+typedef tp_no *tp_arvore;
+
+tp_arvore inicializa_arvore()
+{
+    return NULL;
+}
+
+int arvore_vazia(tp_arvore raiz)
+{
+
+    if (raiz == NULL)
+        return 1;
+    else
+        return 0;
+}
+
+tp_no *aloca_no()//Aloca e retorna o endereco
+{
+    tp_no *no;
+    no=(tp_no*)malloc(sizeof(tp_no));
+    return no;
+}
+
